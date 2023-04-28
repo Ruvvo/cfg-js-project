a1c59e24d50658fb59aac8e9176570092e4c8925
Ruwani <76782671+Ruvvo@users.noreply.github.com>
2023-04-28 04:42:56 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="header">
+        <h1>
+          Buon Viaggio 🍝 Italian Restuarant
+        </h1>
+        <h5 id="tagline"> – Your Tastebuds on a Journey – </h5>
+      </div>
+
+      <div id="content">
+          <h1>Hello world!</h1>
+          <li>🍕</li>
+
+      </div>
+
+      <div id="footer">
+        <p id="info">
+          <i class="fa fa-phone"></i> 07212121 <br></br>
+          <i class="fa fa-shop"></i> 123, Italia Palace, Lala Land<br></br>
+          <i class="fa fa-envelope"></i> info@buonviaggio.com <br></br>
+        </p> 
+      </div>
+    </div>
+  );
+}
+
+export default App;
