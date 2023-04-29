c774c1a7fa027bf684c932c0afca9736d3f02884
Damilola <opemipoolagunju@gmail.com>
2023-04-29 09:07:22 +0100

    feat:homepage created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import platter from "./platter.png";
+import pizzaplate from "./pizzaplate.png";
 
 function App() {
   return (
     <div className="App">
       <div id="header">
-        <h1>
-          Buon Viaggio 🍝 Italian Restaurant
-        </h1>
+        <h1>Buon Viaggio 🍝 Italian Restaurant</h1>
         <h5 id="tagline"> – Your Tastebuds on a Journey – </h5>
       </div>
 
-      <div id="content">
-          <h1>Hello world!</h1>
-          <li>🍕</li>
-
+      <div id="content" className="tagline">
+        <h2 className="firstword">
+          Your tastebuds <br></br>on a journey.
+          <img src={platter} width="500" height="500"></img>
+        </h2>
+        <br></br>
+        <button title="Our Menu" className="button1">
+          Our Menu
+        </button>
+      </div>
+      <div className="split left">
+        <div className="centered">
+          <img src={pizzaplate} width="200" height="200"></img>
+        </div>
+      </div>
+      <div className="split right">
+        <div className="centered">
+          <h3>Our meeting times</h3>
+          <p>Monday-Friday: 5pm - 11pm Closed Sundays</p>
+        </div>
       </div>
 
       <div id="footer">
@@ -21,7 +38,7 @@ function App() {
           <i class="fa fa-phone"></i> 07212121 <br></br>
           <i class="fa fa-shop"></i> 123, Italia Palace, Lala Land<br></br>
           <i class="fa fa-envelope"></i> info@buonviaggio.com <br></br>
-        </p> 
+        </p>
       </div>
     </div>
   );
